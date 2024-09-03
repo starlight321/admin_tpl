8209c7fb181c051088462c6256ae2454efd548bd
starlight321 <358683106@qq.com>
2024-09-03 14:02:37 +0800

    feat: app 列表

--- src/components/Header/RightContent/AppsDropdown.tsx
+++ src/components/Header/RightContent/AppsDropdown.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback } from 'react';
 
 import { AppstoreOutlined } from '@ant-design/icons';
-import { Avatar, Dropdown, Menu } from 'antd';
+import { Avatar, Dropdown } from 'antd';
+import _ from 'lodash';
 
 import { DOMAIN } from '@/utils/enumeration';
 
@@ -18,13 +19,17 @@ let menus: MenuItem[] = [];
 if (menusStr) {
   menus = JSON.parse(menusStr);
   menus.forEach((app) => {
-    projectNotice = projectNotice.concat(app?.children || []);
+    projectNotice = _.uniqBy(
+      projectNotice.concat(app?.children || []),
+      'menuId',
+    );
   });
   projectNotice.unshift({
     name: '工作台',
     path: `${DOMAIN}`,
     Avatar: '',
     type: 'app',
+    menuId: '-1',
   });
 }
 
@@ -32,28 +37,23 @@ const AvatarDropdown: React.FC<GlobalHeaderRightProps> = () => {
   const onMenuClick = useCallback((item: any) => {
     window.location.href = `${item.path}?skipBy=menu`;
   }, []);
-  const menuHeaderDropdown = (
-    <Menu className={styles.menu} selectedKeys={[]}>
-      {projectNotice.map((app: any) => {
-        return app.hideInMenu ? null : (
-          <Menu.Item
-            key={app.name}
-            data-index={app}
-            onClick={() => onMenuClick(app)}
-          >
-            {/* <Avatar size="small" src={app.icon} /> */}
-            <Avatar className={styles.cardAvatar} size="small" src={app.icon}>
-              {app.name.substr(0, 1)}
-            </Avatar>
-            <span className={styles.itemName}>{app.name}</span>
-          </Menu.Item>
-        );
-      })}
-    </Menu>
-  );
+
+ const menuItems = projectNotice
+    .filter((item) => !item.hideInMenu)
+    .map((app: any) => ({
+      key: app.menuId,
+      label: (
+        <div>
+          <Avatar className={styles.cardAvatar} size="small" src={app.icon}>
+            {app.name.substr(0, 1)}
+          </Avatar>
+          <span className={styles.itemName}>{app.name}</span>
+        </div>
+      ),
+    }));
 
   return (
-    <Dropdown overlay={menuHeaderDropdown}>
+    <Dropdown  menu={{ selectedKeys: [], onClick: onMenuClick, items: menuItems }}>
       <span className={`${styles.action} ${styles.account}`}>
         <AppstoreOutlined style={{ paddingRight: '6px' }} />
         <span className={`${styles.name} anticon`}>工作台</span>
